4779dba06aa8528f55db27720e185a888fd7b1e6
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-11-22 22:33:15 +0100

    Learning useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`it's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`it's ${secondary} around here!`);
+  }, [secondary]);
+
   return (
     <>
-      <h1>Current emotion is {emotion}.</h1>
-      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <h1>Current emotion is {emotion} and {secondary}.</h1>
+      <button onClick={() => setEmotion("happy")}>Make happy</button>
       <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
       <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
+      <button onClick={() => setSecondary("crabby")}>Make crabby</button>
     </>
   )
 }
