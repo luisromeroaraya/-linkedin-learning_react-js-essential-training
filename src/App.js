947ed58d5a6e7967d95524b2e0b7fee0df52fdf4
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-11-26 11:09:36 +0100

    Add Outlet to Router so I can have nested pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Contact } from "./pages";
+import {
+  Home,
+  About,
+  Services,
+  CompanyHistory,
+  Location,
+  Events,
+  Contact,
+  NotFound404,
+} from "./pages";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="company-history" element={<CompanyHistory />} />
+          <Route path="location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound404 />} />
       </Routes>
     </div>
   );
--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link, useLocation, Outlet } from "react-router-dom";
 
 export function Home() {
   return (
     <div>
       <h1>[Company Website]</h1>
+      <nav>
+        <Link to="home">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="events">Events</Link>
+        <Link to="contact">Contact</Link>
+      </nav>
     </div>
   );
 }
@@ -12,6 +19,31 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function Services() {
+  return (
+    <div>
+      <h2>Our Services</h2>
+    </div>
+  );
+}
+
+export function CompanyHistory() {
+  return (
+    <div>
+      <h2>Our Company History</h2>
+    </div>
+  );
+}
+
+export function Location() {
+  return (
+    <div>
+      <h1>Our Location</h1>
     </div>
   );
 }
@@ -28,6 +60,16 @@ export function Contact() {
   return (
     <div>
       <h1>[Contact]</h1>
+    </div>
+  );
+}
+
+export function NotFound404() {
+  let location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <h1>[Error 404: Page not found at {location.pathname}]</h1>
     </div>
   );
 }
